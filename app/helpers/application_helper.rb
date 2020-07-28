9cdbf878a6314e9f2ac3dd31318661558ba16108
Andréa Alencar <andreaflether@gmail.com>
2020-07-28 10:48:36 -0300

    Fix a few things in Devise views and application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,32 +1,21 @@
 module ApplicationHelper
   def devise_action(action, controller)
     case action
-    when "new"
+    when 'new', 'create'
       case controller
-      when "passwords"
-        "Recuperar senha"
-      when "registrations"
-        "Criar conta"
-      when "sessions"
-        "Entrar"
+      when 'passwords'
+        'Recuperar senha'
+      when 'registrations'
+        'Criar conta'
+      when 'sessions'
+        'Entrar'
       end
-    when "edit"
+    when 'edit', 'update'
       case controller
-      when "registrations"
-        "Editar informações de conta"
-      when "passwords"
-        "Mudar senha"
-      end
-    when "create"
-      case controller
-      when "registrations"
-        "Criar Conta"
-      when "passwords"
-        "Recuperar senha"
-      when "registrations"
-        "Criar conta"
-      when "sessions"
-        "Entrar"
+      when 'registrations'
+        'Editar informações de conta'
+      when 'passwords'
+        'Mudar senha'
       end
     end
   end
