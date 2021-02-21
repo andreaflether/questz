b0dbd1601275fcece343b3a71efeae3be0f21fa9
Andréa Alencar <andreaflether@gmail.com>
2021-02-21 11:18:35 -0300

    Add factory and specs for Answer model

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: answers
+#
+#  id         :integer          not null, primary key
+#  body       :text             default(""), not null
+#  chosen     :boolean          default(FALSE)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 FactoryBot.define do
   factory :answer do
     chosen { false }
-    content { 'MyText' }
+    body { Faker::Lorem.paragraph_by_chars(number: 200, supplemental: true) }
   end
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,7 +1,20 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: answers
+#
+#  id         :integer          not null, primary key
+#  body       :text             default(""), not null
+#  chosen     :boolean          default(FALSE)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#title' do
+    it { expect(subject).to validate_length_of(:body).is_at_least(15).is_at_most(30000) }
+    it { expect(subject).to validate_presence_of(:body) }
+  end
 end
