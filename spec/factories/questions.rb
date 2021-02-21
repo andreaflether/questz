fa5a6da0e977891d3f4cdce9caa1a0ee8fd1d4ea
Andréa Alencar <andreaflether@gmail.com>
2021-02-20 22:01:36 -0300

    Add factory and specs for Question model

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,6 +1,16 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  status     :integer          default(0)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 FactoryBot.define do
   factory :question do
-    status { false }
-    content { "MyText" }
+    content { Faker::Lorem.paragraph_by_chars(number: 150, supplemental: true) }
+    status { 0 }
   end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,20 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  status     :integer          default(0)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should define_enum_for(:status).
+      with_values([:not_answered, :answered, :closed]) }
+
+  describe '#content' do 
+    it { expect(subject).to validate_length_of(:content).is_at_least(10).is_at_most(200) }
+  end
 end
