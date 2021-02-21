38cf7aaf37c30bb70fb3d5ec11a6977a7a4980a7
Andréa Alencar <andreaflether@gmail.com>
2021-02-20 22:01:25 -0300

    Add validation and enum for Question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  status     :integer          default(0)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Question < ApplicationRecord
+  enum status: {
+    not_answered: 0, 
+    answered: 1,
+    closed: 2
+  }
+
+  validates_length_of :content, in: 10..200
 end
