d9c34b426b3b44d2ed99c5ef2e4eb3a97f325526
Andréa Alencar <andreaflether@gmail.com>
2021-02-20 21:11:45 -0300

    Add validations for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,38 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  name                   :string           default(""), not null
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  username               :string           default(""), not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { maximum: 50, allow_blank: true }
+  validates :username, presence: true,
+                       uniqueness: { case_sensitive: false, allow_blank: true },
+                       format: { with: /\A^[A-Za-z0-9_]+\Z/, allow_blank: true },
+                       length: { minimum: 4, maximum: 20, allow_blank: true }
+
+  validate :username_has_at_least_one_letter, unless: -> { username.blank? }
+
+  def username_has_at_least_one_letter
+    unless username.count('a-zA-Z').positive?
+      errors.add(:username, I18n.t('activerecord.errors.models.user.attributes.username.numeric_only'))
+    end
+  end
 end
