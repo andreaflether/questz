7eed90a17179610b6012646808ea4b4e8e5ac0b9
Andréa Alencar <andreaflether@gmail.com>
2021-04-01 23:10:45 -0300

    Add client-side-validation

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,4 +3,6 @@
 //= require general
 //= require uploads/trix
 //= require trix
+//= require @client-side-validations/client-side-validations/dist/client-side-validations
+//= require @client-side-validations/simple-form/dist/simple-form.bootstrap4
 //= require select2/dist/js/select2.full.min
