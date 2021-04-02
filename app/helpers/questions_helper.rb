204c2472880748bb79ade4ec9a54c052f96c7612
Andréa Alencar <andreaflether@gmail.com>
2021-04-02 20:48:32 -0300

    Improvement in display of Question status

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module QuestionsHelper
+  def status_to_html(status)
+    statuses = { 
+      unanswered: "<i class='fas fa-question-circle'></i>",
+      answered: "<i class='fas fa-check'></i>",
+      closed: "<i class='fas fa-ban'></i>"
+    }
+    "#{statuses[status.to_sym]} &nbsp;#{status.capitalize}"
+  end
 end
