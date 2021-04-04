e37bf9e9dafcc322760e3eeb72bfcc2a21701f7e
Andréa Alencar <andreaflether@gmail.com>
2021-04-04 10:48:58 -0300

    Add public_activity to Question tracked action (create)

--- app/models/question.rb
+++ app/models/question.rb
@@ -26,6 +26,9 @@ class Question < ApplicationRecord
   acts_as_votable
   acts_as_taggable_on :tags
 
+  include PublicActivity::Model
+  tracked only: [:create], owner: ->(controller, model) { model.user }
+
   is_impressionable counter_cache: true
 
   enum status: {
@@ -42,19 +45,26 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   after_create :set_gamification
+  before_destroy :remove_activity
+
+  validates :title, length: { in: 15..150, allow_blank: true }, presence: true
+  validates :body, length: { in: 15..20_000, allow_blank: true }, presence: true
+  validate :tag_list_count
 
   def set_gamification
     gamification = Gamification.new(user)
     gamification.grant_experience_to_user(EXP_FOR_ACTION[:create])
   end
 
-  validates :title, length: { in: 15..150, allow_blank: true }, presence: true
-  validates :body, length: { in: 15..20_000, allow_blank: true }, presence: true
-  validate :tag_list_count
-
   def tag_list_count
     errors.add(:tag_list, 'Please select at least 1 tag to identify your question') if tag_list.count < 1
     errors.add(:tag_list, 'Please enter no more than 5 tags.') if tag_list.count > 5
+  end
+
+  def remove_activity
+    activity = PublicActivity::Activity.find_by(trackable_id: id)
+    activity.destroy if activity.present?
+    true
   end
 
   scope :count_by_status_in_tag, lambda { |tag|
