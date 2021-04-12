4fb3a63723a817a2940f1d63cf2adaae68fc4eca
Andréa Alencar <andreaflether@gmail.com>
2021-04-12 17:02:14 -0300

    Add ChartKick + Groupdate to the application

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,6 +3,8 @@
 //= require general
 //= require uploads/trix
 //= require trix
+//= require chartkick
+//= require Chart.bundle
 //= require @client-side-validations/client-side-validations/dist/client-side-validations
 //= require @client-side-validations/simple-form/dist/simple-form.bootstrap4
 //= require select2/dist/js/select2.full.min
