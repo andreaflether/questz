2bd36c2747c35598a37a483cbe83952e5fcf0c2c
Andréa Alencar <andreaflether@gmail.com>
2021-04-19 21:54:44 -0300

    Add dependent-fields and enum_help gems

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,11 +1,11 @@
 //= require application
-//= require dropzone/dist/dropzone
 //= require general
+//= require dropzone/dist/dropzone
 //= require uploads/trix
 //= require trix
 //= require chartkick
 //= require Chart.bundle
-//= require @client-side-validations/client-side-validations/dist/client-side-validations
-//= require @client-side-validations/simple-form/dist/simple-form.bootstrap4
 //= require select2/dist/js/select2.full.min
 //= require masonry-layout/dist/masonry.pkgd.min
+//= require underscore/underscore-min
+//= require dependent-fields
