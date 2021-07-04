bf1cdcec2e6028f47704f380b4360f17ce6bddae
Andréa Alencar <andreaflether@gmail.com>
2021-07-03 22:29:34 -0300

    Fix points system

--- app/models/point.rb
+++ app/models/point.rb
@@ -17,15 +17,21 @@
 #  index_points_on_user_id  (user_id)
 #
 class Point < Honor::Point
+  MIN_POINTS = 0
+
   after_create :verify_user_experience
 
   def verify_user_experience
     if user.points_total >= user.exp_to_next_level.last
       user.level_up
-    elsif user.exp_to_next_level.member?(user.points_total)
+    elsif user.exp_to_next_level.member?(user.points_total) || min_points_reached?
       nil
     else
       user.level_down
     end
+  end
+
+  def min_points_reached?
+    user.points_total <= user.exp_to_next_level.first || user.points_total <= MIN_POINTS
   end
 end
