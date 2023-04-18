a2e8ea9f3fcac253f2a1fbac1f62e042e78fc3af
Andréa Alencar <andreaflether@gmail.com>
2023-04-17 23:42:18 -0300

    Install libldap2 libs before bundle

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,9 +2,9 @@
 # exit on error
 set -o errexit
 
+sudo apt-get install libldap2-dev
+sudo apt-get install libidn11-dev
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
-sudo apt-get install libldap2-dev
-sudo apt-get install libidn11-dev
